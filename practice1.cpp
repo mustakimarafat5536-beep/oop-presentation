7cf6b98fa1d9e66a38431ef077d175763934be20
mustakimarafat5536-beep <mustakimarafat5536@gmail.com>
2026-04-27 21:51:42 +0900

    Add files via upload

---
+++ practice1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// Class definition
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    // Constructor
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    // Method to display data
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    // Creating object
+    Student s1("Rafi", 23);
+
+    // Calling method
+    s1.display();
+
+    return 0;
+}
